62bb8e035f11fe7c8119f731af7ac7aedd9787ca
lucasmuileboom <muileboomlucas@gmail.com>
2018-10-12 16:12:07 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Procedurally-Generated-Dungeon
+# Dungeon Generator
+*Let goed bij het maken, schrijven en bijhouden van dit document op de **[prestatie-indicatoren](https://drive.google.com/drive/folders/1y8l0Zr4E8b6gYJui_pSzQaoWr-gEr6JN?usp=sharing)**. Deze toon je aan in dit document.*
+
+In deze proefopdracht heb ik in unity met C# een dungeon generator gemaakt
+
+## Software Anaylse
+Ik heb c# gekozen voor dit project omdat dit een taal is waar ik redelijk bekend mee ben, en hier goed mee kan werken. Een grote factor waarom ik voor deze taal gekozen heb is omdat ik later met unity wil werken.
+
+## Leerdoelen
+Wat wil je bereiken met dit project? Formuleer dit kort, krachtig en haalbaar.
+- netter code schrijven in unity C#
+- Het bouwen van een herbruikbare Dungeon generator
+
+## Planning
+Je hebt grofweg 2 weken, hoe deel je deze twee weken in. Wat plan je wanneer om precies te doen?
+
+| | maandag | dinsdag | woensdag | donderdag | vrijdag |
+| --- | --- | --- | --- | --- | --- |
+|week 1 | onderzoek doen naar een Dungeon generator |  beginnen aan roomspawn script|werken aan roomspawn script | beginnen aan deurcheck script||
+|week 2 |werken aan deurcheck | errors fixen | errors fixen  | errors fixen ||readme maken
+
+## Bronnen
+Welke bronnen heb je gebruikt? Zowel youtube filmpjes als artikelen
+
+- [tutorial: dungeon generator](https://www.youtube.com/watch?v=qAf9axsyijY)
---
+++ Scripts/room.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class room : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> doors = new List<GameObject>();
+    private roomspawner _roomspawner;
+
+    void Awake ()
+    {
+        _roomspawner = GameObject.Find("Main Camera").GetComponent<roomspawner>();
+        _roomspawner.check += this.checkneighbours;
+    }
+
+    private void checkneighbours()
+    {
+        if (_roomspawner.Rooms(((int)transform.position.x / _roomspawner.Roomsize()) + _roomspawner.Mapsize() + 1, ((int)transform.position.y / _roomspawner.Roomsize()) + _roomspawner.Mapsize()) == null)
+        {//check right
+            closedoors(doors[4]);
+            closedoors(doors[5]);
+        }
+        if (_roomspawner.Rooms(((int)transform.position.x / _roomspawner.Roomsize()) + _roomspawner.Mapsize() - 1, ((int)transform.position.y / _roomspawner.Roomsize()) + _roomspawner.Mapsize()) == null)
+        {//check left
+            closedoors(doors[0]);
+            closedoors(doors[1]);
+        }
+        if (_roomspawner.Rooms(((int)transform.position.x / _roomspawner.Roomsize()) + _roomspawner.Mapsize(), ((int)transform.position.y / _roomspawner.Roomsize()) + _roomspawner.Mapsize() + 1) == null)
+        {//check up
+            closedoors(doors[2]);
+            closedoors(doors[3]);
+        }
+        if (_roomspawner.Rooms(((int)transform.position.x / _roomspawner.Roomsize()) + _roomspawner.Mapsize(), ((int)transform.position.y / _roomspawner.Roomsize()) + _roomspawner.Mapsize() - 1) == null)
+        {//check down
+            closedoors(doors[6]);
+            closedoors(doors[7]);
+        }
+    }
+    private void closedoors(GameObject door)
+    {
+        door.SetActive(true);
+    }
+}
